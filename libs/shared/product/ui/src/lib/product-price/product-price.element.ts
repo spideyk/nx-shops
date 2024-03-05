e88fcf1c014fa2a2303c338960ffafee374c11e7
“Abhishek <asingh1@avidxchange.com>
2024-03-05 12:41:04 -0600

    fix: remove the error code

--- libs/shared/product/ui/src/lib/product-price/product-price.element.ts
+++ libs/shared/product/ui/src/lib/product-price/product-price.element.ts
@@ -23,10 +23,6 @@ export class ProductPriceElement extends HTMLElement {
     return '$' + (this.value / 100).toFixed(2);
   }
 
-  private get testPrice(): string {
-    return '$' + (this.value / 100).toFixed(2);
-  }
-
   get value(): number {
     return +this.getAttribute(ProductPriceElementAttribute.Value);
   }
